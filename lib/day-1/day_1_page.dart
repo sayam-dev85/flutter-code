057b446649d7abc5b3b55076360d2df5aac1ee7a
Abu Sayam <abusayam.dev@gmail.com>
2025-10-23 23:12:01 +0600

    Image

--- lib/day-1/day_1_page.dart
+++ lib/day-1/day_1_page.dart
@@ -45,6 +45,58 @@ class Day1Page extends StatelessWidget {
                 ],
               ),
             ),
+
+            Icon(
+              Icons.favorite,
+              color: Colors.red,
+              size: 50,
+              semanticLabel: 'A red heart',
+            ),
+
+            Icon(Icons.home, color: Colors.blue, size: 50),
+
+            Image.asset(
+              'assets/images/myimage.png',
+              width: 150,
+              height: 150,
+              fit: BoxFit.cover,
+            ),
+
+            Image.network(
+              'https://images.unsplash.com/photo-1488372759477-a7f4aa078cb6?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGltYWdlfGVufDB8fDB8fHww&auto=format&fit=crop&q=60&w=600',
+              width: 200,
+              height: 200,
+            ),
+
+            /*Image.network(
+              'https://images.unsplash.com/photo-1598214886806-c87b84b7078b?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGltYWdlfGVufDB8fDB8fHww&auto=format&fit=crop&q=60&w=600',
+              loadingBuilder:
+                  (
+                    BuildContext context,
+                    Widget child,
+                    ImageChunkEvent? loadingProgress,
+                  ) {
+                    if (loadingProgress == null) {
+                      return child;
+                    }
+                    return Center(
+                      child: CircularProgressIndicator(
+                        value: loadingProgress.expectedTotalBytes != null
+                            ? loadingProgress.cumulativeBytesLoaded /
+                                  loadingProgress.expectedTotalBytes!
+                            : null,
+                      ),
+                    );
+                  },
+              errorBuilder:
+                  (
+                    BuildContext context,
+                    Object exception,
+                    StackTrace? stackTrace,
+                  ) {
+                    return Icon(Icons.error, color: Colors.red, size: 50);
+                  },
+            ),*/
           ],
         ),
       ),
